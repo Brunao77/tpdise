3d9bee600d362fe92d38fec0b1bb95f1e8aa9b5f
Brunao77 <brucolon@gmail.com>
2022-10-26 15:20:36 -0300

    tpdise

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { sequelize } from "./db/database.js";
 // import "./models/Puesto_Competencia.js";
 
 async function main() {
-  await sequelize.sync({ force: true });
+  await sequelize.sync({ force: false });
   app.listen(3000);
   console.log("Server on port 3000");
 }
