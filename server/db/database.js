fefa90248ee28cffa3fbfefb0f103007cd40c2fe
Brunao77 <brucolon@gmail.com>
2022-10-26 15:20:05 -0300

    tpdise

--- server/db/database.js
+++ server/db/database.js
@@ -1,6 +1,6 @@
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize("tpdise", "postgres", "admin", {
+export const sequelize = new Sequelize("tpdise", "postgres", "tpdisepass", {
   host: "localhost",
   dialect: "postgres",
 });
--- server/gestores/gestorPuesto.js
+++ server/gestores/gestorPuesto.js
@@ -7,18 +7,22 @@ import { sequelize } from "../db/database.js";
 export async function postPuesto(req, res) {
   try {
     const { codigo, nombre, descripcion, empresa, competencias } = req.body;
-    
+
     // Crea instancias de Puesto y Ponderación.
     const newPuesto = new Puesto(codigo, nombre, descripcion, empresa);
     const ponderaciones = competencias.map((competencia) => {
-        return new Ponderacion(codigo, competencia.codigo, competencia.valor);
+      return new Ponderacion(
+        codigo,
+        competencia.codigo,
+        competencia.ponderacion
+      );
     });
 
     // Inserta las instancias creadas en la base de datos, en una transacción.
     await sequelize.transaction(async (t) => {
       await PuestoDAO.create(newPuesto, { transaction: t });
       await PonderacionDAO.bulkCreate(ponderaciones, { transaction: t });
-    })
+    });
 
     res.json(newPuesto);
   } catch (error) {
